56e822de350ddfef4586caddce5c79e4d3ae83b4
Matt Veitas <mveitas@agamatrix.com>
2013-07-20 17:38:22 -0400

    Fixing compile errors

--- src/main/java/shakeandquake/TridentQuake.java
+++ src/main/java/shakeandquake/TridentQuake.java
@@ -1,5 +1,7 @@
 package shakeandquake;
 
+import storm.trident.operation.BaseAggregator;
+import storm.trident.operation.builtin.*;
 import storm.trident.testing.FixedBatchSpout;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
@@ -12,11 +14,6 @@ import storm.trident.TridentState;
 import storm.trident.TridentTopology;
 import storm.trident.operation.BaseFunction;
 import storm.trident.operation.TridentCollector;
-import storm.trident.operation.builtin.Count;
-import storm.trident.operation.builtin.FilterNull;
-import storm.trident.operation.builtin.MapGet;
-import storm.trident.operation.builtin.Sum;
-import storm.trident.planner.processor.StateQueryProcessor;
 import storm.trident.testing.MemoryMapState;
 import storm.trident.tuple.TridentTuple;
 import storm.trident.Stream;
@@ -35,22 +32,26 @@ public class TridentQuake {
     public static class Waffle extends BaseFunction {
         @Override
         public void execute(TridentTuple tuple, TridentCollector collector) {
+            String sentence = tuple.getString(0);
             for(String word: sentence.split(" ")) {
                 collector.emit(new Values(new Boolean(true)));                
                 collector.emit(new Values(new Boolean(false)));                
             }
         }
     }
+
+    static class W_State {
+        int words_quake = 0;
+        int words_base = 0;
+        int tweets_quake = 0;
+        int tweets_base = 0;
+    }
+
     public static class P_Word extends BaseAggregator<W_State> {
             /** expect: (Boolean near, Integer word-count, Integer tweet-count)
              *  emit: (Float p_word_base, Float p_word_quake, Float p_quake)
              */
-            static class W_State {
-                    int words_quake = 0;
-                    int words_base = 0;
-                    int tweets_quake = 0;
-                    int tweets_base = 0;
-            }
+
             public W_State init(Object batchId, TridentCollector collector) {
                     return new W_State();
             }
@@ -65,21 +66,23 @@ public class TridentQuake {
                     }
             }
             public void complete(W_State state, TridentCollector collector) {
-                    Float p_word_base = new Float(float(state.words_base)/state.tweets_base);
-                    Float p_word_quake = new Float(float(state.words_quake)/state.tweets_quake);
-                    Float p_quake = new Float(float(state.tweets_quake) / (state.tweets_base + state.tweets_quake));
+                    Float p_word_base = new Float((state.words_base)/state.tweets_base);
+                    Float p_word_quake = new Float((state.words_quake)/state.tweets_quake);
+                    Float p_quake = new Float((state.tweets_quake) / (state.tweets_base + state.tweets_quake));
                     collector.emit(new Values(p_word_base, p_word_quake, p_quake));
             }
+    }
+
+    static class Q_State {
+        float p_tweet_given_base = 1.0f;
+        float p_tweet_given_quake = 1.0f;
+        float p_quake = 0.0f;
     }
     
     public static class P_Quake extends BaseAggregator<Q_State> {
             /** expect: (Boolean near, Integer word-count, Integer tweet-count)
              */
-            static class Q_State {
-                    float p_tweet_given_base = 1.0;
-                    float p_tweet_given_quake = 1.0;
-                    float p_quake = 0.0;
-            }
+
             public Q_State init(Object batchId, TridentCollector collector) {
                     return new Q_State();
             }
@@ -87,16 +90,16 @@ public class TridentQuake {
                     float p_word_base = tuple.getFloatByField("p_word_base");
                     float p_word_quake = tuple.getFloatByField("p_word_quake");
                     float p_quake = tuple.getFloatByField("p_quake");
-                    state.tweet_given_base *= p_word_base;
-                    state.tweet_given_quake *= p_word_quake;
+                    state.p_tweet_given_base *= p_word_base;
+                    state.p_tweet_given_quake *= p_word_quake;
                     state.p_quake = p_quake;
             }
             public void complete(Q_State state, TridentCollector collector) {
                     float p_quake = state.p_quake;
-                    float p_base = 1.0 - p_quake;
+                    float p_base = 1.0f - p_quake;
                     float p_tweet = ((state.p_tweet_given_base * p_base)
                                     +(state.p_tweet_given_quake * p_quake));
-                    float p_quake_given_tweet = p_tweet_given_quake * p_quake / p_tweet;
+                    float p_quake_given_tweet = state.p_tweet_given_quake * p_quake / p_tweet;
                     collector.emit(new Values(new Float(p_quake_given_tweet)));
             }
     }
@@ -133,13 +136,12 @@ public class TridentQuake {
                 .stateQuery(tweetCounts, new Fields("near"), new MapGet(), new Fields("tweet-count"))
                 .each(new Fields("args"), new Split(), new Fields("word"))
                 .partitionBy(new Fields("near"))
-                .partitionAggregate(new Fields("near", "word"), new FirstN(1, "word"), new Fields("near", "word"))
+                .partitionAggregate(new Fields("near", "word"), new FirstN.FirstNSortedAgg(1, "word", true), new Fields("near", "word"))
                 .stateQuery(wordCounts, new Fields("near", "word"), new MapGet(), new Fields("word-count"))
                 .each(new Fields("word-count"), new FilterNull())
-                .partitionBy(new Fields("word")
+                .partitionBy(new Fields("word"))
                 .partitionAggregate(new Fields("near", "word-count", "tweet-count"), new P_Word(), new Fields("p_word_base", "p_word_quake", "p_quake"))
-                .aggregate(new Fields("p_word_base", "p_word_quake", "p_quake"), new P_Quake(), new Fields("p_quake_given_tweet"))
-                ;
+                .aggregate(new Fields("p_word_base", "p_word_quake", "p_quake"), new P_Quake(), new Fields("p_quake_given_tweet"));
         return topology.build();
     }
     
