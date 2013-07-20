df318827e7473b81f947153892c8e87476be8aa9
Jason Bernardino Alonso <jalonso@lumino.so>
2013-07-20 16:48:06 -0400

    Working toward aggregators for DRPC

--- src/main/java/shakeandquake/TridentQuake.java
+++ src/main/java/shakeandquake/TridentQuake.java
@@ -31,6 +31,64 @@ public class TridentQuake {
             }
         }
     }
+    public static class Waffle extends BaseFunction {
+        @Override
+        public void execute(TridentTuple tuple, TridentCollector collector) {
+            for(String word: sentence.split(" ")) {
+                collector.emit(new Values(new Boolean(true)));                
+                collector.emit(new Values(new Boolean(false)));                
+            }
+        }
+    }
+    public static class P_Word extends BaseAggregator<W_State> {
+            /** expect: (Boolean near, Integer word-count, Integer tweet-count)
+             *  emit: (Float p_word_base, Float p_word_quake, Float p_quake)
+             */
+            static class W_State {
+                    int words_quake = 0;
+                    int words_base = 0;
+                    int tweets_quake = 0;
+                    int tweets_base = 0;
+            }
+            public W_State init(Object batchId, TridentCollector collector) {
+                    return new W_State();
+            }
+            public void aggregate(W_State state, TridentTuple tuple, TridentCollector collector) {
+                    Boolean is_quake = tuple.getBoolean(0);
+                    if(is_quake) {
+                            state.words_quake = tuple.getInteger(1);
+                            state.tweets_quake = tuple.getInteger(2);
+                    } else {
+                            state.words_base = tuple.getInteger(1);
+                            state.tweets_base = tuple.getInteger(2);
+                    }
+            }
+            public void complete(W_State state, TridentCollector collector) {
+                    Float p_word_base = new Float(float(state.words_base)/state.tweets_base);
+                    Float p_word_quake = new Float(float(state.words_quake)/state.tweets_quake);
+                    Float p_quake = new Float(float(state.tweets_quake) / (state.tweets_base + state.tweets_quake));
+                    collector.emit(new Values(p_word_base, p_word_quake, p_quake));
+            }
+    }
+    
+    public static class P_Quake extends BaseAggregator<Q_State> {
+            /** expect: (Boolean near, Integer word-count, Integer tweet-count)
+             */
+            static class Q_State {
+                    float p_tweet_given_base = 0.0;
+                    float p_tweet_given_quake = 0.0;
+                    float p_quake = 0.0;
+                    float p_base = 0.0;
+                    float p_quake_given_tweet =0.0;
+            }
+            public Q_State init(Object batchId, TridentCollector collector) {
+                    return new Q_State();
+            }
+            public void aggregate(Q_State state, TridentTuple tuple, TridentCollector collector) {
+            }
+            public void complete(Q_State state, TridentCollector collector) {
+            }
+    }
     
     public static StormTopology buildTopology(LocalDRPC drpc) {
         // FIXME: This spout needs to be replaced with a KafkaSpout
@@ -59,11 +117,15 @@ public class TridentQuake {
                 .parallelismHint(16);
 
         // FIXME: This DRPC code needs to be fixed to process a tweet correctly
-        topology.newDRPCStream("words", drpc)
+        topology.newDRPCStream("p_quake", drpc)
+                .each(new Fields(), new Waffle(), new Fields("near"))
+                .stateQuery(tweetCounts, new Fields("near"), new MapGet(), new Fields("tweet-count"))
                 .each(new Fields("args"), new Split(), new Fields("word"))
-                .groupBy(new Fields("word"))
-                .stateQuery(wordCounts, new Fields("word"), new MapGet(), new Fields("count"))
-                .each(new Fields("count"), new FilterNull())
+                .groupBy(new Fields("near", "word"))
+                .aggregate(new Fields("word"), new FirstN(1, "word"), new Fields("word-uniq"))
+                .project(new Fields("near", "word", "tweet-count"))
+                .stateQuery(wordCounts, new Fields("near", "word"), new MapGet(), new Fields("word-count"))
+                .each(new Fields("word-count"), new FilterNull())
                 .aggregate(new Fields("count"), new Sum(), new Fields("sum"))
                 ;
         return topology.build();
